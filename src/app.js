2b7f6796498f16e5c0bd9a2f1f843b74cd09348d
tdoran <tomgdoran@gmail.com>
2018-04-27 13:23:29 +0100

    restructured for handlebars Closes #6

--- src/app.js
+++ src/app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 const db = require('./db');
 const controllers = require('./controllers/');
 const path = require('path')
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+app.engine(
+  'hbs',
+  exphbs({
+    extname: 'hbs',
+    layoutsDir: path.join(__dirname, 'views', 'layouts'),
+    partialsDir: path.join(__dirname, 'views', 'partials'),
+    defaultLayout: 'main',
+    // helpers
+  })
+);
 
 app.use(express.static(path.join(__dirname, '..', 'public')));
 app.use(controllers);
